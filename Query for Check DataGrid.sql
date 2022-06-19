4f1bc0badd5dd5be30d00cc77bc2f8f132a505c4
Vasilisa Kashperko <90206134+VasilisaKashperko@users.noreply.github.com>
2022-06-19 15:58:17 +0300

    Final

--- Query for Check DataGrid.sql
+++
@@ -1,4 +0,0 @@
-USE HOAChairmanAssistantDB;
-
-Select Flats.FlatNumber, Owners.Surname, Owners.Name, Owners.Patronymic, PhoneNumbers.MobilePhone, Owners.CurrentDebt, Owners.OwnerStatusId
-from Flats inner join Owners on Flats.FlatId = Owners.FlatId inner join PhoneNumbers on Owners.PhoneNumberId = PhoneNumbers.PhoneNumberId
